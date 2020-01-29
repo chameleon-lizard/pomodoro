86ec5b870c06e8b692edb8313b826b90cc37b3cc
Arina Okshus <nikita@sushko.ru>
2020-01-30 00:33:00 +0300

    Added logging into file, updated readme

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Pomodoro
-
 A simple python pomodoro timer. I was bored and my code sucks.
+
+## Installation:
+Currently you just clone the repo and run the script with python. Nothing fancy yet.
+
+## Usage:
+Pomodoro recieves 3+ parameters. First is the length of worktime (in minutes), second is the length of free time, third and all next parameters are used as the name for your activity.
+If less than 3 parameters are specified, it uses default values - 30 minutes for working, 5 minutes for relaxing and "Random stuff" as the name of activity.
+If you want to end your session, you have to send SIGINT to the process (Ctrl+C). After that it logs your worktime into "data" file in human readable format.
+
+## TODO:
+- Some kind of notification when the cycle ends
+- Script for installing the pomodoro
+- NCurses display
+- MAYBE notes and tasks for the day, if I'll have the time and motivation
+
+Peace!
--- pomodoro.py
+++ pomodoro.py
@@ -3,38 +3,88 @@ import time
 import sys
 import os
 
-def print_cycle(cycles, passed, timer):
+
+def print_cycle(cycles, minutes, timer, activity):
     os.system("clear")
-    print("Current cycles: "+ str(cycles) + ".")
-    print("\n[" + "#" * int(30 * passed // timer[cycles % 2]) + "-" * int(30 - 30 * passed // timer[cycles % 2]) + "]")
+    print("You currently spent " + str(cycles) + " cycles on " + activity + ".")
+    print("\n[" + "#" * int(30 * minutes // timer[cycles % 2]) +
+          "-" * int(30 - 30 * minutes // timer[cycles % 2]) + "]")
     if cycles % 2 == 0:
-        print("\nYou should work. Current time: " + str(passed) + " out of " + str(timer[cycles % 2]) + ".")
+        print("\nTime for work. Current time: " + str(minutes) +
+              " out of " + str(timer[cycles % 2]) + ".")
     else:
-        print("\nYou should rest. Current time: " + str(passed) + " out of " + str(timer[cycles % 2]) + ".")
+        print("\nTime for rest. Current time: " + str(minutes) +
+              " out of " + str(timer[cycles % 2]) + ".")
+
+
+def write_to_file(cycles, minutes, timer, activity):
+    # Opening file
+    file = open("data", "a+")
+    file.seek(0, 0)
+
+    try:
+        # If there already is a line with our activity, we are reading all the lines,
+        # finding the line with our activity, reading numbers from it, updating deleting
+        # the line and writing an updated line to the end of the file in human readable format. 
+        # TODO: find out if there is a better algorythm.
+        content = file.readlines()
+        previous = content.index([s for s in content if activity in s][0])
+        prev_data = []
+        for i in content[previous].split():
+            if i.isnumeric():
+                prev_data.append(int(i))
+
+        prev_data[0] += cycles
+        prev_data[1] += minutes + timer[0] * (cycles // 2 + cycles % 2)
+
+        file.seek(0)
+        for i in content:
+            if activity not in i:
+                file.write(i)
+
+        file.write(activity + ": " +
+                   str(prev_data[0]) + " cycle(s), " + str(prev_data[1]) + " minute(s);\n")
+        file.truncate()
+    # If we catch ValueError or IndexError, it means that we couldn't find the line
+    # with our activity. So, in this case we just create a line in the end.
+    except ValueError:
+        file.seek(0, 2)
+        file.write(activity + ": " + str(cycles) + " cycle(s), " +
+                   str(minutes + timer[0] * (cycles // 2 + cycles % 2)) + " minute(s);\n")
+    except IndexError:
+        file.seek(0, 2)
+        file.write(activity + ": " + str(cycles) + " cycle(s), " +
+                   str(minutes + timer[0] * (cycles // 2 + cycles % 2)) + " minute(s);\n")
+
+    file.close()
+
 
 def signal_handler(sig, frame):
     os.system("clear")
-    print("You worked " + str(cycles // 2 + cycles % 2) + " cycles, totalling " + str(passed + timer[0] * (cycles // 2 + cycles % 2)) + " minutes.")
+    print("You worked on " + activity + " for " + str(cycles // 2 + cycles % 2) +
+          " cycles, totalling " + str(minutes + timer[0] * (cycles // 2 + cycles % 2)) + " minutes.")
+    write_to_file(cycles, minutes, timer, activity)
     sys.exit(0)
 
-# 30 minutes for work, 15 minutes for rest by default
-timer = [30, 15]
-cycles = 0
-passed = 0
 
-if len(sys.argv) == 3:
+# 30 minutes for work, 5 minutes for rest by default.
+timer = [30, 5]
+cycles = 0
+minutes = 1
+activity = "random stuff"
+
+if len(sys.argv) >= 4:
     timer = [int(sys.argv[1]), int(sys.argv[2])]
+    activity = " ".join(sys.argv[3:])
 
 signal.signal(signal.SIGINT, signal_handler)
 
 while True:
-    passed = 0
-    while passed != timer[cycles % 2]:
-        print_cycle(cycles, passed, timer)
-        passed += 1
+    # Endless loop, to stop you just send SIGINT.
+    minutes = 1
+    while minutes <= timer[cycles % 2]:
+        # Every loop is 1 minute
+        print_cycle(cycles, minutes, timer, activity)
+        minutes += 1
         time.sleep(60)
-
-    os.system("clear")
-    print_cycle(cycles, passed, timer)
     cycles += 1
-    time.sleep(1)
